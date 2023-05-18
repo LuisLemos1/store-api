bc6b2769c3a1b203ad7ae57d1a590f5dc499d40e
Luis Lemos <luisgouveialemos@gmail.com>
2023-05-18 14:37:15 +0100

    Added Project for XUnit tests
    Added Project for XUnit tests
    Created a template for the XUnit tests for further elaboration

---
+++ StoreAPI/StoreAPI.Tests/DataTests/IDataContextUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreAPI.Tests.DataTests
+{
+    public class Data_Customers
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+
+    public class Data_Orders
+    {
+        [Fact]
+        public void Test2()
+        {
+
+        }
+    }
+
+    public class Data_Products
+    {
+        [Fact]
+        public void Test2()
+        {
+
+        }
+    }
+
+    public class Data_SaveDB
+    {
+        [Fact]
+        public void Test2()
+        {
+
+        }
+    }
+}
---
+++ StoreAPI/StoreAPI.Tests/DomainTests/IServiceDomainUnitTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreAPI.Tests.DomainTests
+{
+    public class Domain_GetAll
+    {
+        [Fact]
+        public void Domain_GetAll_()
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+
+    public class Domain_GetCustomerById
+    {
+        [Fact]
+        public void Test2()
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+
+    public class Domain_AddCustomer
+    {
+        [Fact]
+        public void Test2()
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+
+    public class Domain_UpdateCustomer
+    {
+        [Fact]
+        public void Test2()
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+
+    public class Domain_DeleteCustomer
+    {
+        [Fact]
+        public void Test2()
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+
+    public class Domain_GetCustomers
+    {
+        [Fact]
+        public void Test2()
+        {
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
